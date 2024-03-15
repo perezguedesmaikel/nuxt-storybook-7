701493f4e8237ee62f07506653af6e6652d82bd9
Enmanuel Ponce <enmanuelponcedeleon@gmail.com>
2024-03-15 01:37:50 -0400

    ok

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/storybook',
+  ],
+  storybook: {
+    url: 'http://localhost:6006',
+    storybookRoute: '.storybook',
+    port: 6006,
+  },
+})
